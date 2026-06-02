2d170f4bfbc80c564092508e4f8d2e9456fcbaf9
SuryaKAkurati <surya.k.akurati@gmail.com>
2026-06-02 12:51:37 -0400

    testing with full text notes - successful

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 from retrieval import search
 
-results = search("transformers", k=5)
+results = search(""" Helps differentiating multiple documents within same corpus
+	- Compares how often a term appears in a specific document compared to its general frequency across the entire collection of documents
+	- Formula used to calculate IDF is idf(t) = log(N / df(t))
+	- TF - IDF = tf(t, d) * log(N / df(t))
+	- High TF-IDF score means word appears more often in one document but rarely in others.
+
+
+#ml""", k=5)
 
 for r in results:
     print(r)
